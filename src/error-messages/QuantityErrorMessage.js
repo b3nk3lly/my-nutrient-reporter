0c909151b922d6868dee39920657a98e636e51bc
b3nk3lly <benjkelly98@gmail.com>
2023-02-18 15:21:07 -0400

    Added switch statement

--- src/error-messages/QuantityErrorMessage.js
+++ src/error-messages/QuantityErrorMessage.js
@@ -2,19 +2,16 @@ import QuantityValidity from "../enums/QuantityValidity";
 import GenericErrorMessage from "./GenericErrorMessage";
 
 const getMessage = (validity) => {
-	if (validity === QuantityValidity.EMPTY) {
-		return "Please enter a quantity.";
+	switch (validity) {
+		case QuantityValidity.EMPTY:
+			return "Please enter a quantity.";
+		case QuantityValidity.NAN:
+			return "Please enter a number.";
+		case QuantityValidity.NON_POSITIVE:
+			return "Please enter a positive number.";
+		default:
+			return "";
 	}
-
-	if (validity === QuantityValidity.NAN) {
-		return "Please enter a number.";
-	}
-
-	if (validity === QuantityValidity.NON_POSITIVE) {
-		return "Please enter a positive number.";
-	}
-
-	return "";
 };
 
 function QuantityErrorMessage({ validity }) {
