5f300f3f8fbdc8ee4cff2a50adaf96341a06422f
b3nk3lly <benjkelly98@gmail.com>
2023-02-26 13:51:55 -0400

    Added form validation

--- src/report/ReportGenerator.js
+++ src/report/ReportGenerator.js
@@ -30,7 +30,45 @@ const getNutrientAmountPerFood = async (nutrient, food) => {
 };
 
 function ReportGenerator({ meals, nutrients }) {
+	/**
+	 * Returns true if and only if all meals contain at least one food,
+	 * all foods have a quantity, and at least one nutrient is selected.
+	 * If any of these criteria are not met, an alert is displayed.
+	 */
+	const validate = () => {
+		for (let meal of meals) {
+			let mealName = meal.name == "" ? "Untitled Meal" : meal.name;
+
+			if (meal.foods.length === 0) {
+				window.alert(
+					`Please enter at least one food for meal ${mealName}.`
+				);
+				return false;
+			}
+
+			for (let food of meal.foods) {
+				if (!food.quantity || food.quantity === 0) {
+					window.alert(
+						`Please enter a quantity for ${food.description} in ${mealName}`
+					);
+					return false;
+				}
+			}
+		}
+
+		if (nutrients.length === 0) {
+			window.alert("Please select at least one nutrient.");
+			return false;
+		}
+
+		return true;
+	};
+
 	const generateReport = async () => {
+		if (!validate()) {
+			return;
+		}
+
 		let workbook = new Excel.Workbook();
 		let worksheet = workbook.addWorksheet("");
 
