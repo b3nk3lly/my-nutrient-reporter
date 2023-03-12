0c29381b4a5a4668788714368e3723ea5ab566bd
b3nk3lly <benjkelly98@gmail.com>
2023-03-12 16:54:18 -0300

    Added React to import statements

--- src/meal/ServingUnit.jsx
+++ src/meal/ServingUnit.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { React, useState, useEffect } from 'react';
 import { Select, MenuItem } from '@mui/material';
 import ReducerActions from '../enums/ReducerActions';
 
